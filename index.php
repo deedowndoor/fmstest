709ba4096ddd503e33856c88408f1f2fb6158d3d
deedowndoor <inborndee@gmail.com>
2025-07-01 11:11:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<http>
+<body>
+
+<h2> User register database </h2>
+
+<form action="dataall.php" method="post">
+  <div>
+    <label for="id">รหัสผู้ใช้งาน:</label>
+    <input type="text" id="id"><br>
+  </div>
+  <div>
+    <label for="name">ชื่่อผู้ใช้:</label>
+    <input type="text" id="name"><br>
+
+</div>
+  <div>
+    <label for="email">อีเมล:</label>
+    <input type="text" id="email"><br>
+
+</div>
+<div>
+    <label for="pass">รหัสผ่าน:</label>
+    <input type="text" id="pass"><br>
+
+</div>
+ <div style="background-color: lightblue; padding: 20px;">
+    <input type="submit" value="ส่ง">
+    <input type="reset" value="รีเซ็ต">
+</div>
+
+
+ 
+</form>
+
+<body>
+
+
+</http>
